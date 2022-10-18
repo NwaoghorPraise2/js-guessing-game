ea2ce9e096b09582203bff1673adb92f10adb43c
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-10-18 22:51:02 +0200

    Setting-Up-Data-Structures

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+document.querySelector(".getForm-Data").addEventListener("submit", submit);
+
+
+function submit(e) {
+    e.preventDefault();
+    alert("working");
+   windows.location.href = "/views/main-game/game.html";
+    return rediret;
+}
