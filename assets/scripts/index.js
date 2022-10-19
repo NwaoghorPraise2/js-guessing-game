35fff4795f83a76633325b715bbab6b2c90d7726
Replit user <>
2022-10-19 10:40:53 +0000

    Added-icons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,9 @@
+
 document.querySelector(".getForm-Data").addEventListener("submit", submit);
 
 
 function submit(e) {
     e.preventDefault();
     alert("working");
-   windows.location.href = "/views/main-game/game.html";
-    return rediret;
+window.location.assign('https://js-guessing-game-1.nwaoghorpraise.repl.co/views/main-game/game.html');
 }
