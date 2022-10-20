02702bda25c0da18fe127c9a35304f96deb4c601
Nwaoghor Praise <52567426+NwaoghorPraise2@users.noreply.github.com>
2022-10-20 04:37:59 -0700

    UI-Semi-Done

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,5 +5,5 @@ document.querySelector(".getForm-Data").addEventListener("submit", submit);
 function submit(e) {
     e.preventDefault();
     alert("working");
-window.location.assign('https://js-guessing-game-1.nwaoghorpraise.repl.co/views/main-game/game.html');
+window.location.assign('views/main-game/game.html');
 }
