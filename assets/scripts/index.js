ad8a9b5bae7a39fc0b3f516472f6c4c0bffad970
NwaoghorPraise2 <chukunwaoghor@gmail.com>
2022-10-23 22:06:52 +0200

    Syncing-Files

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,5 +5,5 @@ document.querySelector(".getForm-Data").addEventListener("submit", submit);
 function submit(e) {
     e.preventDefault();
     alert("working");
-window.location.assign('views/main-game/game.html');
+window.location.assign('views/main-game');
 }
